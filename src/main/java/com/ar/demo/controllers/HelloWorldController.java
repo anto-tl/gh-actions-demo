1435b8ce23060c874e4ab729cd2cab72e76b0296
AR <anto@titaniamlabs.com>
2021-09-13 16:28:49 +0530

    Initial GHA script

---
+++ src/main/java/com/ar/demo/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ar.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello! Welcome to GH Actions demo app";
+    }
+}
